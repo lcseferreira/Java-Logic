c54b6d9fb6f7f961bcfe88133d94147f4f622b42
Lucas Ferreira <lcseferreira@hotmail.com>
2021-09-16 08:25:07 -0300

    Declaring variables

---
+++ src/academy/devdojo/classes/A02DeclaringVariables.java
@@ -0,0 +1,16 @@
+package academy.devdojo.classes;
+
+public class A02DeclaringVariables {
+
+    public static void main(String[] args) {
+
+        // Declaring variable of int type
+        int age = 23;
+
+        // Declaring variable of float type
+        float weight = 78.5F;
+
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight);
+    }
+}

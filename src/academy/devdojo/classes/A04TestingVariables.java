04b3c8d36f761050c90230d0d120c83f7b5e6202
Lucas Ferreira <lcseferreira@hotmail.com>
2021-09-16 08:35:05 -0300

    Testing variables

---
+++ src/academy/devdojo/classes/A04TestingVariables.java
@@ -0,0 +1,23 @@
+package academy.devdojo.classes;
+
+public class A04TestingVariables {
+
+    public static void main(String[] args) {
+
+        char male = 'M';
+        char female = 'F';
+
+        String phrase = "String is a Wrapper class.";
+
+        boolean condition0 = false;
+        boolean condition1 = true;
+
+        System.out.println(male);
+        System.out.println(female);
+        System.out.println(phrase);
+
+        // In sout, the boolean values returned a String
+        System.out.println(condition0);
+        System.out.println(condition1);
+    }
+}
